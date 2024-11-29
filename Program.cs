5564a658e056ce92ac890fd0ae1f4effb122d9c4
Anas Ghazi <anasghazi@Anass-MacBook-Air-2.local>
2024-11-29 21:56:54 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main()
+    {
+        var options = new ChromeOptions();
+        using (var driver = new ChromeDriver(options))
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            System.Console.WriteLine("Google opened successfully!");
+            driver.Quit();
+        }
+    }
+}
