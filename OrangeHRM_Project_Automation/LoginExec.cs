93fb8047793aa917012a5a277ec16ac2f72c2edd
seher imtiaz <100214571+seherim@users.noreply.github.com>
2024-12-04 19:07:34 +0500

    driver

--- OrangeHRM_Project_Automation/LoginExec.cs
+++ OrangeHRM_Project_Automation/LoginExec.cs
@@ -8,24 +8,23 @@ using System.Windows;
 
 namespace OrangeHRM_Project_Automation
 {
+   
     [TestClass]
     public class LoginExec
     {
-
-        IWebDriver driver = new ChromeDriver();
-
+        LoginPage loginPage = new LoginPage();
         [TestMethod]
         public void Login_Valid_TC001()
         {
             string url = "https://opensource-demo.orangehrmlive.com/";
             string username = "Admin";
             string password = "admin123";
-            Login(url, username, password);
-            string dashboardHeader = driver.FindElement(By.XPath("//h6[text()='Dashboard']")).Text;
+            loginPage.Login(url, username, password);
+            string dashboardHeader = CorePage.driver.FindElement(By.XPath("//h6[text()='Dashboard']")).Text;
             Assert.AreEqual("Dashboard", dashboardHeader, "Login failed, Dashboard not found!");
 
             Task.Delay(1000);
-            driver.Close();
+            CorePage.driver.Close();
 
         }
 
@@ -35,23 +34,14 @@ namespace OrangeHRM_Project_Automation
             string url = "https://opensource-demo.orangehrmlive.com/";
             string username = "Admins";
             string password = "admin!23";
-            Login(url, username, password);
-            string dashboardHeader = driver.FindElement(By.XPath("//p[text()='Invalid credentials']")).Text;
+            loginPage.Login(url, username, password);
+            string dashboardHeader = CorePage.driver.FindElement(By.XPath("//p[text()='Invalid credentials']")).Text;
             Assert.AreEqual("Invalid credentials", dashboardHeader, "Login failed, Dashboard not found!");
-
-            Task.Delay(1000);
-            driver.Close();
+            CorePage.driver.Close();
 
         }
 
-        public void Login(string url, string username, string password)
-        {
-            driver.Url = url;
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-            driver.FindElement(By.Name("username")).SendKeys(username);
-            driver.FindElement(By.Name("password")).SendKeys(password);
-            driver.FindElement(By.XPath("//button[@type='submit']")).Click();            
-        }
+
+
     }
 }
