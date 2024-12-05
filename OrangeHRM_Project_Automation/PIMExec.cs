f2da021b1e71620f63d8837a6bab7e82fe95fe3e
seher imtiaz <100214571+seherim@users.noreply.github.com>
2024-12-05 18:49:36 +0500

    changes

---
+++ OrangeHRM_Project_Automation/PIMExec.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace OrangeHRM_Project_Automation
+{
+    [TestClass]
+    public class PIMExec
+    {
+        PIMPage pimPage = new PIMPage();
+        LoginPage loginPage = new LoginPage();
+
+        [TestMethod]
+        public void AddEmployee_TC001()
+        {
+            CorePage.SeleniumInit();
+
+            string url = "https://opensource-demo.orangehrmlive.com/";
+            loginPage.Login(url, "Admin", "admin123");
+
+            pimPage.NavigateToPIM();
+
+            pimPage.AddEmployee("John", "Doe");
+
+            bool isEmployeeAdded = CorePage.driver.FindElement(By.XPath("//div[contains(text(),'Successfully Saved')]")).Displayed;
+            Assert.IsTrue(isEmployeeAdded, "Employee was not added successfully!");
+
+            CorePage.driver.Close();
+        }
+
+        [TestMethod]
+        public void SearchEmployee_TC002()
+        {
+            CorePage.SeleniumInit();
+
+            string url = "https://opensource-demo.orangehrmlive.com/";
+            loginPage.Login(url, "Admin", "admin123");
+
+            pimPage.NavigateToPIM();
+
+            pimPage.SearchEmployee("John");
+
+            bool isEmployeeFound = CorePage.driver.FindElement(By.XPath("//div[contains(text(),'john')]")).Displayed;
+            Assert.IsTrue(isEmployeeFound, "Employee was not found!");
+
+            CorePage.driver.Close();
+        }
+    }
+}
---
+++ OrangeHRM_Project_Automation/PIMPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace OrangeHRM_Project_Automation
+{
+    class PIMPage : CorePage
+    {
+        public void NavigateToPIM()
+        {
+            driver.FindElement(By.XPath("//span[text()='PIM']")).Click();
+        }
+
+        public void AddEmployee(string firstName, string lastName)
+        {
+            driver.FindElement(By.XPath("//button[text()=' Add ']")).Click();
+
+            driver.FindElement(By.Name("firstName")).SendKeys(firstName);
+            driver.FindElement(By.Name("lastName")).SendKeys(lastName);
+
+            driver.FindElement(By.XPath("//button[text()=' Save ']")).Click();
+        }
+
+        public void SearchEmployee(string name)
+        {
+
+            driver.FindElement(By.XPath("//input[@placeholder='Type for hints...']")).SendKeys(name);
+
+
+            driver.FindElement(By.XPath("//button[text()=' Search ']")).Click();
+        }
+
+    }
+}
