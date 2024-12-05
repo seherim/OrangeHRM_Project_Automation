795a8fa4a65499f8154874bbf27a1eea4d74e99b
seher imtiaz <100214571+seherim@users.noreply.github.com>
2024-12-05 20:56:42 +0500

    update

--- OrangeHRM_Project_Automation/RecruitPage.cs
+++ OrangeHRM_Project_Automation/RecruitPage.cs
@@ -9,22 +9,18 @@ namespace OrangeHRM_Project_Automation
 {
     class RecruitPage : CorePage
     {
-        // Navigate to Recruitment Page
         public void NavigateToRecruitmentPage()
         {
             driver.FindElement(By.XPath("//span[text()='Recruitment']")).Click();
         }
 
-        // Add Job Vacancy
         public void AddJobVacancy()
         {
         }
 
-        // Search for a Candidate
         public void SearchCandidate()
         { }
 
-        // Approve/Reject Candidate
         public void ApproveOrRejectCandidate()
         {
         }
